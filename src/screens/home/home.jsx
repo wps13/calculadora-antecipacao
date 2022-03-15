98159b44a6a662d18f7d455cc370f4be6623df5c
Willane Paiva <willanepaiva@hotmail.com>
2022-03-15 09:28:59 -0300

    feat: implement initial home screen

---
+++ src/screens/home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./home.styles.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>home</h1>
+    </div>
+  );
+};
+
+export default Home;
