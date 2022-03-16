c5acb850f2e43a8b0d71edd0be8d1dcbbf04dd47
Willane Paiva <willanepaiva@hotmail.com>
2022-03-15 21:25:11 -0300

    feat: add initial home layout

---
+++ src/screens/home/components/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+import * as CardComponent from "./card/card";
+import * as InputSectionComponent from "./input-section/input-section";
+import * as AntecipationSectionComponent from "./antecipation-section/antecipation-section";
+import * as DividerComponent from "./divider/divider";
+
+const Card = CardComponent.default;
+const InputSection = InputSectionComponent.default;
+const AntecipationSection = AntecipationSectionComponent.default;
+const Divider = DividerComponent.default;
+
+export { Card, InputSection, AntecipationSection, Divider };
--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -2,10 +2,34 @@ import React from "react";
 
 import "./home.styles.scss";
 
+import { MainTitleUI, SectionTitleUI } from "../../ui";
+import { Card, InputSection, AntecipationSection, Divider } from "./components";
+
 const Home = () => {
   return (
     <div className="home">
-      <h1>home</h1>
+      <Card>
+        <div className="card-container">
+          <div className="card-container__inputs">
+            <MainTitleUI title="Simule sua antecipação" />
+            <form>
+              <InputSection
+                saleAmount="1"
+                installments="2"
+                mdrPercentage="3.2"
+                onInstallmentsChanged={() => {}}
+                onSaleAmountChanged={() => {}}
+                onMdrPercentageChanged={() => {}}
+              />
+            </form>
+          </div>
+          <div className="card-container__results">
+            <SectionTitleUI title="VOCÊ RECEBERÁ:" />
+            <Divider />
+            <AntecipationSection />
+          </div>
+        </div>
+      </Card>
     </div>
   );
 };
