18c4259c46bec0f5a1be74982aa99a855f957440
Willane Paiva <willanepaiva@hotmail.com>
2022-03-17 09:18:21 -0300

    style: improve antecipation section display

--- src/screens/home/components/antecipation-section/antecipation-section.jsx
+++ src/screens/home/components/antecipation-section/antecipation-section.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 
 import { TextBlockUI } from "../../../../ui";
 
+import "./antecipation-section.styles.scss";
+
 const AntecipationSection = (props) => {
   const {
     tomorrowAmount,
@@ -12,12 +14,12 @@ const AntecipationSection = (props) => {
   } = props;
 
   return (
-    <>
+    <div className="antecipation-section">
       <TextBlockUI label="Amanhã:" amount={tomorrowAmount} />
       <TextBlockUI label="Em 15 dias:" amount={fifteenDaysAmount} />
       <TextBlockUI label="Em 30 dias:" amount={thirtyDaysAmount} />
       <TextBlockUI label="Em 90 dias:" amount={ninetyDaysAmount} />
-    </>
+    </div>
   );
 };
 
